8591369c9b511d362c2df57d34be800e94cf709c
william0143 <48636214+william0143@users.noreply.github.com>
2019-03-17 15:35:30 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,24 +2,348 @@
 export const storeProducts = [
   {
     id: 1,
-    title: "Classic Handbag",
+    title: "Aldactone 100mg 96 Tablets Spironolactone",
     img: "img/product-1.jpg",
-    price: 4936,
-    company: "Chanel",
+    price: 1,
+    company: "ARIS",
     info:
-      "Grained Calfskin & Gold-Tone Metal",
+      "Spironolactone is used to treat high blood pressure and heart failure. This medication is also used to treat low potassium levels and conditions in which the body is making too much of a natural chemical (aldosterone). Spironolactone is known as a 'water pill'.",
     inShoppingBag: false,
     count: 0,
     total: 0
   },
   {
     id: 2,
-    title: "Soho Small Leather Disco Bag",
+    title: "Androcur 50mg 50 tablets",
     img: "img/product-2.jpg",
-    price: 1049,
-    company: "Gucci",
+    price: 1,
+    company: "BAYER",
     info:
-      "A compact shoulder bag with a leather tassel zipper pull. Sized to fit the necessities. Made in our light, natural grain leather.",
+      "Androcur 50 mg containing 50 tablets or Androcur 100 mg containing 30 tablets.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Casodex Bicalutamide 50 mg/ 150 mg Anti-Androgen",
+    img: "img/product-3.jpg",
+    price: 1,
+    company: "AstraZeneca",
+    info:
+      "Casodex (Bicalutamide) is an anti-androgen medicine primarily used to treat prostate cancer. Casodex 50 mg (28 tablets) and Casodex 150 mg (28 tablets) are available.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Climara 50 & Climara Forte Transdermal Patch",
+    img: "img/product-4.jpg",
+    price: 1,
+    company: "BAYER",
+    info:
+      "Climara is a skin patch that contains an estrogen called estradiol. Estrogens are female sex hormones produced by the ovaries. Estrogens are necessary for many processes in the body. Climara skin patches are used to treat certain symptoms of menopause such as hot flashes, and vaginal dryness, burning, and irritation. Climara is also used to prevent postmenopausal osteoporosis, or to treat ovarian disorders. Climara 50: Each Patch 3.9 mg  Patch Size -12.5 cm2  4 patches ea box. Climara Forte: Each Patch- 7.6 mg   Patch size- 25 cm2   4 patches ea. box",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Climen Estrogen + Anti-androgen Combo",
+    img: "img/product-5.jpg",
+    price: 1,
+    company: "BAYER",
+    info:
+      "Climen tablets is packed in blister packs, containing tablets of color coated pill in white and pink tablets: white tablets of 2 mg estradiol valerate and pink tablets of 2 mg estradiol valerate plus 1 mg cyproterone acetate. Pack size-21 tablets",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Cyclo-Progynova 2mg 2 boxes Estrogen + Anti-androgen combo",
+    img: "img/product-6.jpg",
+    price: 1,
+    company: "BAYER",
+    info:
+      "White Tablets: 2mg estradiol valerate. Pale Brown Tablets: 500 micrograms norgestrel 2mg estradiol valerate. Pack size - 21 tablets",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Diane 35 2 boxes estrogen + anti-androgen combo",
+    img: "img/product-7.jpg",
+    price: 1,
+    company: "BAYER",
+    info:
+      "Contains a combination of two ingredients: cyproterone and ethinyl estradiol. Cyproterone belongs to a group of medications known as antiandrogens. Ethinyl estradiol belongs to a group of medications known as estrogens. 21 tablets per pack.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Dilaprost 5 mg Finasteride",
+    img: "img/product-8.jpg",
+    price: 1,
+    company: "",
+    info:
+      "Dilaprost (Finasteride) is a medication used mainly to treat an enlarged prostate.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "Dogmatil 200 mg Sulpiride 24 tablets Breast Milk Production",
+    img: "img/product-9.jpg",
+    price: 1,
+    company: "Sanofi Aventis",
+    info:
+      "Has been used therapeutically as an antidepressant, antipsychotic, and as a digestive aid. Sulpiride is also used to induce breast milk production. Dogmatil 200 mg contains 24 tablets in each box.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 10,
+    title: "Estrofem Estradiol 2mg 3 Boxes",
+    img: "img/product-10.jpg",
+    price: 1,
+    company: "Sanofi Aventis",
+    info:
+      "Estrofem tablets contain oestradiol 2mg, an active form of the female hormone oestrogen, and are used to replace the loss of oestrogen at menopause, relieving menopausal symptoms. Estrofem also stimulates the development of female sexual characteristics for male to female transsexuals.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 11,
+    title: "Humira Adalimumab 40mg",
+    img: "img/product-11.jpg",
+    price: 1,
+    company: "",
+    info:
+      "humira, adalimumab, rheumatoid arthritis, psoriatic arthritis, ankylosing spondylitis, Crohn's disease, ulcerative colitis, chronic psoriasis, hidradenitis suppurativa, juvenile idiopathic arthritis",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 12,
+    title: "Inhaler Ventolin Salbutamol 100 Mcg 200 Doses Asthma Treatment",
+    img: "img/product-12.jpg",
+    price: 1,
+    company: "",
+    info:
+      "Ventolin is an inhaler, which relieves acute symptoms of asthma. The inhaler contains salbutamol, an active ingredient which reduces the constriction in your airways which causes asthma. 100 Mcg 200 Doses. Larger order discount available.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 13,
+    title: "Mesigyna Norethisterone and Estadiol Valerate Injection",
+    img: "img/product-13.jpg",
+    price: 1,
+    company: "BAYER",
+    info:
+      "Mesigyna describes the use of a once-a-month injection (once-a-month injectable) that contains an estrogenic and gestagenic component as an active ingredient for the production of a pharmaceutical agent for contraception and simultaneous hormone replacement therapy for perimenopausal and premenopausal women. Each Box Contains ONE (1) Ready Made Injector For Use.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 14,
+    title: "Microgynon 30 2 boxes estrogen + progesterone combo",
+    img: "img/product-14.jpg",
+    price: 1,
+    company: "BAYER",
+    info:
+      "Microgynon 30 is a combined pill, and one of the most common and widespread forms of contraception. It contains two hormones (ethinylestradiol and levonorgestrel) that are similar to the oestrogen and progesterone produced by your body. Pack size - 63 tablets",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 15,
+    title: "Modafinil Modiwake 100 mg",
+    img: "img/product-15.jpg",
+    price: 1,
+    company: "Generico",
+    info:
+      "Modiwake is a mood-improving and mood-brightening psycho-stimulant. It enhances wakefulness, attention capacity and vigilince. 30 tablets each 100 mg per pack.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 16,
+    title: "Motis 10 mg 30 tablets Domperidone 10 boxes",
+    img: "img/product-16.jpg",
+    price: 1,
+    company: "Motis",
+    info:
+      "Domperidone is used to treat nausea and vomiting, complaints of the stomach, which occur with delayed emptying of the stomach. Domperidone is used to induce breast milk production.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 17,
+    title: "Progestan 100 mg/200 mg Progesterone Capsules",
+    img: "img/product-17.jpg",
+    price: 1,
+    company: "Kocak Farma",
+    info:
+      "Progesterone is used to cause menstrual periods in women who have not yet reached menopause but are not having periods due to a lack of progesterone in the body. It is also used to prevent overgrowth in the lining of the uterus in postmenopausal women who are receiving estrogen hormone replacement therapy. Progestan 100 mg 30 capsules each box. Progestan 200 mg 30 capsules each box.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 18,
+    title: "Progestan 50 mg Injection 5 ampules",
+    img: "img/product-18.jpg",
+    price: 1,
+    company: "Kocak Farma",
+    info:
+      "Progesterone is used to cause menstrual periods in women who have not yet reached menopause but are not having periods due to a lack of progesterone in the body. It is also used to prevent overgrowth in the lining of the uterus in postmenopausal women who are receiving estrogen hormone replacement therapy. Progestan 50 mg 5 ampules in each box.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 19,
+    title: "Proscar Finasteride 5 mg 2 boxes",
+    img: "img/product-19.jpg",
+    price: 1,
+    company: "MSD",
+    info:
+      "Proscar Finastride is a medication used mainly to treat an enlarged prostate or scalp hair loss in men. 28 Tablets per box. Each tablet 5 mg.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 20,
+    title: "Proviron 40 Tabs Mesterolone Androgen 2 boxes",
+    img: "img/product-20.jpg",
+    price: 1,
+    company: "BAYER",
+    info:
+      "20 Tablets, 25mg each box.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 21,
+    title: "Tarlusal 5 mg Provera Medroxyprogesterone acetate 5 boxes",
+    img: "img/product-21.jpg",
+    price: 1,
+    company: "DEVA",
+    info:
+      "5 mg each 12 tablets per box",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 22,
+    title: "Testosterone Capsules",
+    img: "img/product-22.jpg",
+    price: 1,
+    company: "DEVA",
+    info:
+      "30 capsules 40 mg ea.",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 23,
+    title: "Testosterone Gel",
+    img: "img/product-23.jpg",
+    price: 1,
+    company: "BESINS",
+    info:
+      "Testosterone Gel",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 24,
+    title: "Testosterone Injections",
+    img: "img/product-24.jpg",
+    price: 1,
+    company: "BESINS",
+    info:
+      "Testosterone Injections",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 25,
+    title: "Yasmin 2 boxes",
+    img: "img/product-25.jpg",
+    price: 1,
+    company: "BESINS",
+    info:
+      "(drospirenone and ethinyl estradiol) prevents ovulation (the release of an egg from an ovary) and also cause changes in your cervical and uterine lining, making it harder for sperm to reach the uterus and harder for a fertilized egg to attach to the uterus. Pack size - 21 Tablets",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 26,
+    title: "Genotropin GoQuick HGH",
+    img: "img/product-26.jpg",
+    price: 1,
+    company: "Pfizer",
+    info:
+      "Human growth hormone 12 mg",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 27,
+    title: "Botulinum Toxin Type A Botox",
+    img: "img/product-27.jpg",
+    price: 1,
+    company: "Allergan",
+    info:
+      "Botox injectibles",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 28,
+    title: "Prolutex 25mg Progestrone",
+    img: "img/product-28.jpg",
+    price: 1,
+    company: "IBSA",
+    info:
+      "Injectible progestrone",
+    inShoppingBag: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 29,
+    title: "Crinone Progestrone Vaginal Gel",
+    img: "img/product-29.jpg",
+    price: 1,
+    company: "Merck Serono",
+    info:
+      "Progestrone gel",
     inShoppingBag: false,
     count: 0,
     total: 0
