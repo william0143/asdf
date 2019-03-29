3a096a9487304974d078992a2ec936788c1f8eb5
william0143 <48636214+william0143@users.noreply.github.com>
2019-03-28 22:54:20 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,20 @@ import {ButtonContainer} from './Button';
 class Footer extends Component {
     render() {
         return (
-            <FooterWrapper className="navbar navbar-expand-sm bg-pink navbar-light px-sm-5">                
-                <ul className="navbar-nav align-items-center">
-                    <li className="nav-item ml-5">
-                        <span className="footerstuff">Help us keep serving our community and friends by donating to our BTC wallet at:     1JeNnzkHowzsD9guTvSfztQqifpTiniud8</span>
+            // <FooterWrapper className="footer bg-pink px-sm-5">                
+                
+            //         <li className="nav-item ml-5">
+            //             <span className="footerstuff">Help us keep serving our community and friends by donating to our BTC wallet at:     1JeNnzkHowzsD9guTvSfztQqifpTiniud8</span>
+            //         </li>
+                             
+            // </FooterWrapper>
+            <div>
+                <div class="menu">
+                <li class="item">
+                        <a class="donation">Help us keep serving our community and friends by donating to our BTC wallet at:     1JeNnzkHowzsD9guTvSfztQqifpTiniud8<br/> Thank You!</a>
                     </li>
-                </ul>                 
-            </FooterWrapper>
+                </div>
+            </div>
         );
     }
 }
@@ -23,7 +30,11 @@ const FooterWrapper = styled.nav`
         text-align: center
         font-size: 1.5rem;
         text-transform: capitalize;
-    }
+        bottom: -50px;
+        height: 50px;
+        left: 0;                
+        right: 0;
+    };   
     border-top:0.20rem solid var(--hotPink);
 `
 //color:var(--mainWhite) !important;
